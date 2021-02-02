db375697fef61d47455b0785eb7451d0351c1d03
alisom.martins <alisom.martins@vitreo.com.br>
2021-02-02 11:41:44 -0300

    finalizado teste

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -7,28 +7,52 @@ const jwtHandler = require('../auth/verifyJwt');
 
 const customerService = require('../services/customerService');
 
+
 module.exports = (app) => {
 
-    app.get('/customer', async (req, res, next) => {
+    app.get('/customer', async(req, res, next) =>{
         try {
-            //const id = req.params.id;
-            const customer = await customerService.getCustomerById();
-            if (customer)
+            const customer = await customerService.getCustomer();
+            if(customer)
                 res.json(customer);
         } catch (error) {
             next(appError(500, error));
-        }
+        }     
     })
 
-    app.post('/customer', async(req,res,next)=>{
+    app.post('/customer', async(req, res, next) =>{
         try {
             const body = req.body;
-            const customer = await customerService.addCustomer(body);
-            if (customer)
-                res.json(customer);
+            const customer = await customerService.insertCustomer(body);
+            if(customer)
+                res.json('ok!');
         } catch (error) {
             next(appError(500, error));
-        }
+        }     
+    })
+
+    app.put('/customer/:id', async(req, res, next) =>{
+        try {
+            const body = req.body;
+            const id = req.params.id;
+            const customer = await customerService.updateCustomer(body, id);
+            if(customer)
+                res.json('ok!');
+        } catch (error) {
+            next(appError(500, error));
+        }     
+    })
+
+
+    app.del('/customer/:id', async(req, res, next) =>{
+        try {
+            const id = req.params.id;
+            const customer = await customerService.deleteCustomer(id);
+            if(customer)
+                res.json('ok!');
+        } catch (error) {
+            next(appError(500, error));
+        }     
     })
 
 }
--- services/customerService.js
+++ services/customerService.js
@@ -2,15 +2,27 @@
 
 const CustomerModel = require('../models/customer_model');
 
-const getCustomerById = async () => {
+const getCustomer = async () => {
     return await CustomerModel.find({}).lean();
 }
 
-const addCustomer = async(customer) =>{
+const insertCustomer = async (customer) => {
     return await CustomerModel.insertMany(customer);
 }
 
+const updateCustomer = async (customer, id) => {
+    return await CustomerModel.updateOne({
+        _id:id
+    }, customer, {upsert:true});
+}
+
+const deleteCustomer = async (id) =>{
+    return await CustomerModel.findOneAndDelete({_id:id});
+}
+
 module.exports = {
-    getCustomerById,
-    addCustomer
+    getCustomer,
+    insertCustomer,
+    updateCustomer,
+    deleteCustomer
 }
